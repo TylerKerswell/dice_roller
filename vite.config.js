e09b5a9a4d7e57736ffd9a9f7b48bf987e285ee4
Tyler Kerswell <tylerkerswell@gmail.com>
2026-01-17 17:17:21 -0800

    added build

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,12 @@
-export default {
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  base: '/',
   server: {
     open: true
+  },
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets'
   }
-};
+});
